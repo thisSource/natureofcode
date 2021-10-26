4270ebec1deaba7665e7f9f0d2e1896cf1f52757
alxSource <alexander.hoglund@gmail.com>
2021-10-26 20:11:20 +0200

    perlin1

---
+++ pages/perlinnoise.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import dynamic from "next/dynamic";
+
+// Will only import `react-p5` on client-side
+const Sketch = dynamic(() => import("react-p5").then((mod) => mod.default), {
+  ssr: false
+});
+
+export default (props) => {
+  let start = 0;
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(400, 400).parent(canvasParentRef);
+  };
+
+  const draw = (p5) => {
+    let xOff = start;
+    let increment = 0.01;
+    p5.background(0);
+    p5.noFill();
+    p5.beginShape();
+
+    for (let x = 0; x < p5.width; x++) {
+      p5.stroke(255);
+      var y = p5.noise(xOff) * p5.height;
+      p5.vertex(x, y);
+      xOff += increment;
+    }
+    p5.endShape();
+    start += increment;
+  };
+
+  // Will only render on client-side
+  return (
+    <div className="">
+      <Sketch setup={setup} draw={draw} />;
+    </div>
+  );
+};
---
+++ pages/randomwalkerperlinnoise.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import dynamic from "next/dynamic";
+
+// Will only import `react-p5` on client-side
+const Sketch = dynamic(() => import("react-p5").then((mod) => mod.default), {
+  ssr: false
+});
+
+export default (props) => {
+  let xOff = 0;
+  let yOff = 1;
+
+
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(400, 400).parent(canvasParentRef);
+    p5.background(150,150,150);
+
+  };
+
+  const draw = (p5) => {
+    let x = p5.map(p5.noise(xOff), 0, 1, 0, p5.width);
+    let y = p5.map(p5.noise(yOff), 0, 1, 0, p5.width);
+
+    xOff += 0.01;
+    yOff += 0.01;
+    p5.fill(200,x,y)
+    p5.noStroke()
+    p5.ellipse(x, y, 10, 10);
+  };
+
+  // Will only render on client-side
+  return (
+    <div className="">
+      <Sketch setup={setup} draw={draw} />;
+    </div>
+  );
+};
